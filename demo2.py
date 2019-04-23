5f10373930ac6494808b8b85b1479a7dcb4c7ee1
Daniel T <danieltinizaray@gmail.com>
2019-04-22 19:24:26 -0500

    correcion-de-estilo

--- demo2.py
+++ demo2.py
@@ -12,8 +12,8 @@ variable = sys.argv[0]
 dato1 = sys.argv[1]
 dato2 = sys.argv[2]
 
-salidaSuma = int(dato1)+int(dato2)
-salidaMultiplicacion = int(dato1)*int(dato2)
+salidaSuma = int(dato1) + int(dato2)
+salidaMultiplicacion = int(dato1) * int(dato2)
 
 print("La suma es :	%s" % salidaSuma)
 print("La multiplicacion es:	%s" % salidaMultiplicacion)
